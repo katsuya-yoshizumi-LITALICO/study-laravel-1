bb9837cc7153c1721298b48a4b504e1d86e6ab20
katsuya-yoshizumi-LITALICO <124348262+katsuya-yoshizumi-LITALICO@users.noreply.github.com>
2025-10-29 16:30:09 +0900

    add php doc comments

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,20 +6,33 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    // 投稿一覧表示
+    /**
+     * 投稿一覧を表示する
+     *
+     * @return \Illuminate\View\View 投稿一覧ビュー
+     */
     public function index()
     {
         $posts = Post::orderBy('created_at', 'desc')->get();
         return view('posts.index', compact('posts'));
     }
 
-    // 新規投稿フォーム表示
+    /**
+     * 新規投稿フォームを表示する
+     *
+     * @return \Illuminate\View\View 投稿フォームビュー
+     */
     public function create()
     {
         return view('posts.create');
     }
 
-    // 投稿保存処理
+    /**
+     * 投稿を保存する
+     *
+     * @param \Illuminate\Http\Request $request リクエストオブジェクト
+     * @return \Illuminate\Http\RedirectResponse 投稿一覧へのリダイレクト
+     */
     public function store(Request $request)
     {
         $request->validate([
@@ -29,13 +42,28 @@ class PostController extends Controller
         Post::create($request->only(['name', 'body']));
         return redirect()->route('posts.index');
     }
-    // API: 投稿一覧
+
+    /**
+     * @api {get} /api/posts 投稿一覧取得
+     * @apiName GetPosts
+     * @apiGroup Posts
+     * @apiSuccess {Object[]} posts 投稿データ
+     * @apiSuccess {Number} posts.id 投稿ID
+     * @apiSuccess {String} posts.name 投稿者名
+     * @apiSuccess {String} posts.body 本文
+     * @apiSuccess {String} posts.created_at 投稿日時
+     */
     public function apiIndex()
     {
         return response()->json(Post::orderBy('created_at', 'desc')->get());
     }
 
-    // API: 投稿保存
+    /**
+     * 投稿を保存する
+     *
+     * @param \Illuminate\Http\Request $request リクエストオブジェクト
+     * @return \Illuminate\Http\JsonResponse 保存結果のJSON
+     */
     public function apiStore(Request $request)
     {
         $request->validate([
