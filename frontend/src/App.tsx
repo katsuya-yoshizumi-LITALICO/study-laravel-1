063a4c46c6f0c21d8ef4f69919b68deb92786ce3
katsuya-yoshizumi-LITALICO <124348262+katsuya-yoshizumi-LITALICO@users.noreply.github.com>
2025-10-29 16:29:42 +0900

    add frontend formatter settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 type Post = {
   id: number;
